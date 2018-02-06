2c277de8639102eb1bfa06a8c938c5bb59d8187e
bhaskaranvinod <35401740+bhaskaranvinod@users.noreply.github.com>
2018-02-06 10:53:02 +0530

    Add files via upload

---
+++ volumeofsphere.py
@@ -0,0 +1,7 @@
+import math
+diameter = input ("enter the diameter of sphere\n")
+int(diameter)
+r = int(diameter) / 2
+volume = (4/3)*math.pi*r*r
+float(volume)
+print ("Volume of sphere is", float(volume))
